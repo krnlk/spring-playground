8be4d3a85d698521780a4157b809b66d9b2acd3c
krnlk <95872735+krnlk@users.noreply.github.com>
2026-01-16 13:47:43 +0100

    Initial project config.

---
+++ src/main/java/krnlk/spring_playground/SpringPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package krnlk.spring_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/krnlk/spring_playground/SpringPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package krnlk.spring_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
